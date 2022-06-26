6849b2704fcb30f0d88f5c24b716e2b55353b149
ZAKARIA RABHI <61620962+zrabhi@users.noreply.github.com>
2022-06-26 16:10:45 +0100

    starting to work on bonus part

---
+++ philo_bonus/parsing/ft_philo_utilis.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_philo_utilis.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zakaria <zakaria@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/25 13:55:20 by zakaria           #+#    #+#             */
+/*   Updated: 2022/06/25 18:23:53 by zakaria          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../philosophers.h"
+
+bool  ft_init(int ac , char **av, t_table *table)
+{
+     if(ac == 1)
+        {
+            red();
+            printf("\n\n YOUR ENTERED NOTHING PLEASE ENTER YOUR ARGUMENTS\n\n");
+            return(false);
+        }
+    table->number_of_philosophers = ft_atoi(av[1]);
+    table->time_to_die = ft_atoi(av[2]);
+    table->time_to_eat = ft_atoi(av[3]);
+    table->time_to_sleep = ft_atoi(av[4]);
+    if(ac == 6)
+        table->number_of_times_each_philosopher_must_eat = ft_atoi(av[5]);
+    return(true);
+}
---
+++ philo_bonus/src/ft_creat_lst.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_creat_lst.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zakaria <zakaria@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/25 13:29:50 by zakaria           #+#    #+#             */
+/*   Updated: 2022/06/25 18:40:47 by zakaria          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+t_data *ft_newnode(int id)
+{
+    t_data *philo_data;
+
+    philo_data = malloc(sizeof(t_data));
+    if(!philo_data)
+        return(NULL);
+    philo_data->next = NULL;
+    philo_data->previous = NULL;
+    philo_data->id   = id;
+    philo_data->is_dead  = 0;
+    philo_data->fork_num = 0;
+    philo_data->t_eat = 0;
+    philo_data->philo_age = ft_get_time();
+    return (philo_data);
+}
+
+t_table *ft_creatphilo_data(t_table **table, t_data *data)
+{
+    t_data *tmp;
+
+    tmp = NULL;
+    if(!data)
+        return (NULL);
+    if(!(*table)->head)
+    {
+        (*table)->head = data;
+        (*table)->head->next = (*table)->head;
+        (*table)->head->previous = (*table)->head;
+        (*table)->head->table = (*table);
+    }
+    else
+    {
+        tmp = (*table)->head->previous;
+        (*table)->head->previous = data;
+        data->next = (*table)->head;
+        data->previous = tmp;
+        tmp->next = data;
+        data->table = (*table);
+    }
+    return(*table);
+}
+
+
+void    ft_get_philo_data(t_table *table)
+{
+    int i;
+
+    i = -1;
+    while(++i < table->number_of_philosophers)
+           table = ft_creatphilo_data(&table, ft_newnode(i + 1));
+}
